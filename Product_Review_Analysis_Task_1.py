fe797ff9ebe4f54bcb9a61666395b5793c935e13
Balbisaur <albi3479@yahoo.com>
2024-04-17 10:49:12 -0500

    first commit

---
+++ Product_Review_Analysis_Task_1.py
@@ -0,0 +1,21 @@
+
+
+reviews = ("This product is really good. I'm impressed with its quality.")
+cap = reviews.replace('good', 'GOOD')
+print(cap)
+
+reviews = ("The performance of this product is excellent. Highly recommended!")
+cap = reviews.replace('excellent', 'EXCELLENT')
+print(cap)
+
+reviews = ("I had a bad experience with this product. It didn't meet my expectations.")
+cap = reviews.replace('bad', 'BAD')
+print(cap)
+
+reviews = ("Poor quality product. Wouldn't recommend it to anyone.")
+cap = reviews.replace('Poor', 'POOR')
+print(cap)
+
+reviews = ("The product was average. Nothing extraordinary about it.")
+cap = reviews.replace('average', 'AVERAGE')
+print(cap)
---
+++ example.py
@@ -0,0 +1,4 @@
+jungle = 'There is a TiGeR in my jungle'
+lower_jungle = jungle.lower()
+tiger = lower_jungle.find('tiger')
+print(tiger)
