a404bace00348e17e162205ae78e318e67dbf3d0
Gerardo Ramirez <166074441+Balbisaur@users.noreply.github.com>
2024-04-19 22:47:04 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,1 @@
-jungle = 'There is a TiGeR in my jungle'
-lower_jungle = jungle.r()
-tiger = lower_jungle.find('tiger')
-print(tiger)
 
-#I was using this as an example for .upper aswell to try to upper case a certain word in a string, but kept getting my whole string uppercase
-#then I remembered about .replace lol
