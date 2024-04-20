673dcfc2782f3aee2d4f9c4ac919676e81471f06
Gerardo Ramirez <166074441+Balbisaur@users.noreply.github.com>
2024-04-19 22:46:24 -0500

    Update Product_Review_Analysis_Task_1.py

--- Product_Review_Analysis_Task_1.py
+++ Product_Review_Analysis_Task_1.py
@@ -1,21 +1,10 @@
+reviews = ( "This product is really good. I'm impressed with its quality.", "The performance of this product is excellent. Highly recommended!", "I had a bad experience with this product. It didn't meet my expectations.", "Poor quality product. Wouldn't recommend it to anyone.", "The product was average. Nothing extraordinary about it.")
 
+keywords = ("good", "excellent", "bad", "poor", "average")
 
-reviews = ("This product is really good. I'm impressed with its quality.")
-cap = reviews.replace('good', 'GOOD')
-print(cap)
+for review in reviews:
+    for keyword in keywords:
+        if keyword in review:
+            highlighted = review.replace(keyword, keyword.upper())
+            print(highlighted)
 
-reviews = ("The performance of this product is excellent. Highly recommended!")
-cap = reviews.replace('excellent', 'EXCELLENT')
-print(cap)
-
-reviews = ("I had a bad experience with this product. It didn't meet my expectations.")
-cap = reviews.replace('bad', 'BAD')
-print(cap)
-
-reviews = ("Poor quality product. Wouldn't recommend it to anyone.")
-cap = reviews.replace('Poor', 'POOR')
-print(cap)
-
-reviews = ("The product was average. Nothing extraordinary about it.")
-cap = reviews.replace('average', 'AVERAGE')
-print(cap)
